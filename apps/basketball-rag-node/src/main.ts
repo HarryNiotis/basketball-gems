a6b8f9b300f546e469e782eac5e193f4266d234d
Harry Niotis <chniotis@gmail.com>
2026-03-03 15:19:12 +0000

    RAG: Update entry point

--- apps/basketball-rag-node/src/main.ts
+++ apps/basketball-rag-node/src/main.ts
@@ -1,39 +1,14 @@
 import dotenv from 'dotenv';
 import { initStore } from './store';
-import { agent } from './agent';
-import readline from 'readline/promises';
 
 dotenv.config();
 
 async function main() {
   console.log('Starting the basketball RAG node...');
-  // const rl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
 
   await initStore();
 
-  // const inputMessage = 'Πες μου για τον Παναθηναϊκό';
-
-  // const agentInputs = {
-  //   messages: [{ role: 'user', content: inputMessage }],
-  // };
-
-  // const stream = await agent.stream(agentInputs, {
-  //   streamMode: 'values',
-  // });
-
-  // for await (const step of stream) {
-  //   console.log({
-  //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //     // @ts-expect-error
-  //     messages: JSON.stringify(step.messages[step.messages.length - 1]),
-  //   });
-  //   // const lastMessage = step.messages[step.messages.length - 1];
-  //   // console.log(`[${lastMessage.role}]: ${lastMessage.content}`);
-  //   console.log('-----\n');
-  // }
+  console.log('Basketball RAG node is ready to handle queries.');
 }
 
 main();
